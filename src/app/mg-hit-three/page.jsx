4c5e7d8f36762a4c2e4e493982004d510a532e68
Adebest <aadebesta@gmail.com>
2024-10-24 17:55:22 +0100

    changes

---
+++ src/app/mg-hit-three/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      mg hit three
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/pnh-post-test/page.jsx
@@ -0,0 +1,39 @@
+
+// import PnhPostTest from "@/components/PnhPostTest";
+
+import PnhPostTest from "@/components/PnhPostTest";
+
+
+
+    // const apiKey = process.env.NEXT_PUBLIC_TYPEFORM_TOKEN;
+    
+    const apiKey =  process.env.NEXT_PUBLIC_TYPEFORM_TOKEN;
+    const endpoint = `https://api.typeform.com/forms/dGNnJsgE`;
+    //  const token = process.env.NEXT_PUBLIC_HYGRAPH_TOKEN;
+
+const page = async() => {
+
+ const response = await fetch(endpoint, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${apiKey}`,
+                'Content-Type': 'application/json'
+            }
+        });
+        // console.log(response);
+        
+        
+        // if (!response.ok) {
+          //     throw new Error(`Error: ${response.statusText}`);
+          // }
+          
+          const data = await response.json();
+          console.log(data);
+
+  return (
+ <PnhPostTest dataFromTypeForm={data}  />
+
+  )
+}
+
+export default page
