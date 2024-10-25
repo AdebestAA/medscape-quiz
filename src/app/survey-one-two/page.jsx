8758a32cedee0757e95d0f9b10deb856fedde6cf
Adebest <aadebesta@gmail.com>
2024-10-25 11:56:27 +0100

    removed it

--- src/app/survey-one-two/page.jsx
+++ src/app/survey-one-two/page.jsx
@@ -3,7 +3,6 @@
 import SurveyOneTwo from "@/components/SurveyOneTwo";
 
 
-    const apiKey = 'tfp_7jNiNB7pBsroNt9fvjLHSWMLpkYRjABEpfDH888YB2Qp_hkTkjbhU58VN';
     const endpoint = `https://api.typeform.com/forms/UPVdh9Zd`;
 
 const page = async() => {
@@ -21,7 +20,7 @@ const page = async() => {
         // if (!response.ok) {
           //     throw new Error(`Error: ${response.statusText}`);
           // }
-          
+    
           const data = await response.json();
           console.log(data);
 
