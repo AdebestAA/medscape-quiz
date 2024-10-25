85bb3d11cdbb6bc31d0ead3c17548ec06995074e
Adebest <aadebesta@gmail.com>
2024-10-25 12:32:48 +0100

    now again

--- src/app/survey-three/page.jsx
+++ src/app/survey-three/page.jsx
@@ -1,33 +1,33 @@
 
 
-// import SurveyThree from "@/components/SurveyThree";
+import SurveyThree from "@/components/SurveyThree";
 
 
    
-//     const endpoint = `https://api.typeform.com/forms/U5Kt7eg9`;
+    const endpoint = `https://api.typeform.com/forms/U5Kt7eg9`;
 
-// const page = async() => {
+const page = async() => {
 
-//  const response = await fetch(endpoint, {
-//             method: 'GET',
-//             headers: {
-//                 'Authorization': `Bearer ${process.env.TYPEFORM_TOKEN}`,
-//                 'Content-Type': 'application/json'
-//             }
-//         });
-//         console.log(response);
+ const response = await fetch(endpoint, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${process.env.TYPEFORM_TOKEN}`,
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log(response);
         
         
-//         // if (!response.ok) {
-//         //       throw new Error(`Error: ${response.statusText}`);
-//         //   }
+        // if (!response.ok) {
+        //       throw new Error(`Error: ${response.statusText}`);
+        //   }
           
-//           const data = await response.json();
-//           // console.log(data); 
+          const data = await response.json();
+          // console.log(data); 
 
-//   return (
-//     <SurveyThree dataFromTypeForm={data} />
-//   )
-// }
+  return (
+    <SurveyThree dataFromTypeForm={data} />
+  )
+}
 
-// export default page
+export default page
